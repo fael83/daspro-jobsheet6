e30c1027b927ed8388d4b110005401af7f9313af
fael83 <rafaellokajaya@gmail.com>
2024-10-08 17:24:41 +0700

    Experiment 1

---
+++ Selection2Exp1244107060039.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Selection2Exp1244107060039 {
+
+    public static void main(String[] args) {
+        Scanner input244107060039 = new Scanner(System.in);
+        System.out.print("Input year = ");
+        int year = input244107060039.nextInt();
+        if (year %4 == 0) 
+            if (year %100 != 0 )
+                    System.out.println("Leap year");
+        else 
+            System.out.println("Not a leap year");
+        }
+    }
